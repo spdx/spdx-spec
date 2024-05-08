001ddb918c0a70497967f828d8aaa015819fae80
Joshua Watt <JPEWhacker@gmail.com>
2024-05-08 17:28:22 -0600

    Add annex describing how to cross reference documents
    Adds a markdown document with examples that explains how to refer to
    elements across documents
    
    Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- bin/check-examples.sh
+++ bin/check-examples.sh
@@ -25,25 +25,43 @@ done
 
 T=$(mktemp -d)
 
+check_schema() {
+    check-jsonschema \
+        -v \
+        --schemafile https://spdx.org/schema/3.0.0/spdx-json-schema.json \
+        "$1"
+}
+
+check_model() {
+    pyshacl \
+        -s https://spdx.org/rdf/3.0.0/spdx-model.ttl \
+        -e https://spdx.org/rdf/3.0.0/spdx-model.ttl \
+        "$1"
+}
+
+
 for f in $THIS_DIR/../docs/annexes/*.md; do
-    if ! grep -q '```json' $f; then
+    if ! grep -q '^```json' $f; then
         continue
     fi
     echo "Checking $f"
-    echo "" > $T/temp.json
+    DEST=$T/$(basename $f)
+    mkdir -p $DEST
 
-    if ! grep -q '@context' $f; then
-        cat >> $T/temp.json <<HEREDOC
+    cat $f | awk -v DEST="$DEST" 'BEGIN{flag=0} /^```json/, $0=="```" { if (/^---$/){flag++} else if ($0 !~ /^```.*/ ) print $0 > DEST "/doc-" flag ".spdx.json"}'
+
+    echo "[" > $DEST/combined.json
+
+    for doc in $DEST/*.spdx.json; do
+        if ! grep -q '@context' $doc; then
+            mv $doc $doc.fragment
+            cat >> $doc <<HEREDOC
 {
     "@context": "https://spdx.org/rdf/3.0.0/spdx-context.jsonld",
     "@graph": [
 HEREDOC
-    fi
-
-    cat $f | awk '/^```json/, $0=="```" {if ($0 !~ /^```.*/ ) print}' >> $T/temp.json
-
-    if ! grep -q '@context' $f; then
-        cat >> $T/temp.json <<HEREDOC
+            cat $doc.fragment >> $doc
+            cat >> $doc <<HEREDOC
         {
             "type": "CreationInfo",
             "@id": "_:creationInfo",
@@ -60,17 +78,15 @@ HEREDOC
     ]
 }
 HEREDOC
-    fi
+        fi
+        check_schema $doc
+        cat $doc >> $DEST/combined.json
+        echo "," >> $DEST/combined.json
+    done
 
-    check-jsonschema \
-        -v \
-        --schemafile https://spdx.org/schema/3.0.0/spdx-json-schema.json \
-        $T/temp.json
+    echo "{}]" >> $DEST/combined.json
 
-    pyshacl \
-        -s https://spdx.org/rdf/3.0.0/spdx-model.ttl \
-        -e https://spdx.org/rdf/3.0.0/spdx-model.ttl \
-        $T/temp.json
+    check_model $DEST/combined.json
 done
 
 
