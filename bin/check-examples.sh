047d3add8167ec2f309439a07c3e553eca48c3c3
Arthit Suriyawongkul <arthit@gmail.com>
2026-01-19 21:17:19 +0000

    Use spdx3-validate instead of pyshacl
    Also print all URLs to assist debugging
    
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- bin/check-examples.sh
+++ bin/check-examples.sh
@@ -4,6 +4,7 @@
 # documentation
 #
 # SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: Copyright 2024-2026 The SPDX Contributors
 #
 # It was the case that we use MAJOR.MINOR.PATCH (3-part) version number
 # (e.g., "3.0.1") in IRI of terms and RDF files.
@@ -13,44 +14,63 @@
 
 set -e
 
-THIS_DIR="$(dirname "$0")"
 SPDX_VERSION="3.0.1"
+
+THIS_DIR="$(dirname "$0")"
+MD_DIR=docs/annexes
+JSON_DIR=examples/jsonld
+
 SPDX_VERSION_MAJOR_MINOR="$(echo "$SPDX_VERSION" | cut -d. -f1,2)"  # 3.0.1 -> 3.0; 3.0 -> 3.0
 SCHEMA_URL="https://spdx.org/schema/${SPDX_VERSION_MAJOR_MINOR}/spdx-json-schema.json"
 RDF_URL="https://spdx.org/rdf/${SPDX_VERSION_MAJOR_MINOR}/spdx-model.ttl"
 CONTEXT_URL="https://spdx.org/rdf/${SPDX_VERSION_MAJOR_MINOR}/spdx-context.jsonld"
 
+# print validation setup
+echo "Checking examples in"
+echo "Snippets         : $MD_DIR"
+echo "Files            : $JSON_DIR"
+echo "SPDX version     : $SPDX_VERSION"
+echo "(major.minor)    : $SPDX_VERSION_MAJOR_MINOR"
+echo "Schema           : $SCHEMA_URL"
+echo "Schema resolved  : $(curl -I "$SCHEMA_URL" 2>/dev/null | grep -i "location:" | awk '{print $2}')"
+echo "RDF              : $RDF_URL"
+echo "RDF resolved     : $(curl -I "$RDF_URL" 2>/dev/null | grep -i "location:" | awk '{print $2}')"
+echo "Context          : $CONTEXT_URL"
+echo "Context resolved : $(curl -I "$CONTEXT_URL" 2>/dev/null | grep -i "location:" | awk '{print $2}')"
+echo "$(check-jsonschema --version)"
+echo "spdx3-validate version: $(spdx3-validate --version)"
+echo ""
+
 check_schema() {
+    echo "Checking schema (check-jsonschema): $1"
     check-jsonschema \
-        -v \
+        --verbose \
         --schemafile $SCHEMA_URL \
         "$1"
 }
 
-check_model() {
-    pyshacl \
-        -s $RDF_URL \
-        -e $RDF_URL \
-        "$1"
+check_spdx() {
+    echo "SPDX 3 Validating (spdx3-validate): $1"
+    spdx3-validate --json $1
 }
 
 # Check examples in JSON files in examples/jsonld/
-if [ "$(ls $THIS_DIR/../examples/jsonld/*.json 2>/dev/null)" ]; then
-    for f in $THIS_DIR/../examples/jsonld/*.json; do
-        echo "Checking $f"
+if [ "$(ls $THIS_DIR/../$JSON_DIR/*.json 2>/dev/null)" ]; then
+    for f in $THIS_DIR/../$JSON_DIR/*.json; do
         check_schema $f
-        check_model $f
+        echo ""
+        check_spdx $f
+        echo ""
     done
 fi
 
-TEMP=$(mktemp -d)
-
 # Check examples in inline code snippets in Markdown files in docs/annexes/
-for f in $THIS_DIR/../docs/annexes/*.md; do
+TEMP=$(mktemp -d)
+for f in $THIS_DIR/../$MD_DIR/*.md; do
     if ! grep -q '^```json' $f; then
         continue
     fi
-    echo "Checking $f"
+    echo "Extract snippets from $f"
     DEST=$TEMP/$(basename $f)
     mkdir -p $DEST
 
@@ -58,7 +78,8 @@ for f in $THIS_DIR/../docs/annexes/*.md; do
     cat $f | awk -v DEST="$DEST" 'BEGIN{flag=0} /^```json/, $0=="```" { if (/^---$/){flag++} else if ($0 !~ /^```.*/ ) print $0 > DEST "/doc-" flag ".spdx.json"}'
 
     # Combine all JSON code snippets into a single file, with SPDX context and creation info.
-    echo "[" > $DEST/combined.json
+    COMBINED_JSON = $DEST/__combined.jso
+    echo "[" > $COMBINED_JSON
 
     for doc in $DEST/*.spdx.json; do
         if ! grep -q '@context' $doc; then
@@ -88,11 +109,12 @@ HEREDOC
 HEREDOC
         fi
         check_schema $doc
-        cat $doc >> $DEST/combined.json
-        echo "," >> $DEST/combined.json
+        echo ""
+        cat $doc >> $COMBINED_JSON
+        echo "," >> $COMBINED_JSON
     done
 
-    echo "{}]" >> $DEST/combined.json
-
-    check_model $DEST/combined.json
+    echo "{}]" >> $COMBINED_JSON
+    check_spdx $COMBINED_JSON
+    echo ""
 done
